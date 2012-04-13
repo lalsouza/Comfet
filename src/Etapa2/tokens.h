2c5ffad05d3f4b1da1a2b64ddaa65c89b3e6bedb
Alain André <alainandreamaral@gmail.com>
2012-04-13 02:36:13 -0300

    Implementação Etapa 2
    Implementação dos arquivos (Etapa 2)

---
+++ src/Etapa2/tokens.h
@@ -0,0 +1,28 @@
+/* Lista dos tokens, com valores constantes associados.
+ * Este arquivo tera vocacao a ser incluido em varios outros.
+ * A escolha dos valores constantes eh totalmente arbitraria.
+ * Cada valor deve ser distinto...
+ */
+
+#define INT 100
+#define DOUBLE 101
+#define FLOAT 102
+#define CHAR 103
+
+#define QUOTE 110
+#define DQUOTE 111
+
+#define LE 120
+#define GE 121
+#define EQ 122
+#define NE 123
+#define AND 124
+#define OR 125
+
+#define IF 130
+#define THEN 131
+#define ELSE 132
+#define WHILE 133
+#define IDF 134
+#define INT_LIT 135
+#define F_LIT 136
